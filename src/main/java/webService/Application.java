31540b4e7db53a135265d40640e4bbde4b32f04c
yonay cabrera lopez <yonaycl@gmail.com>
2016-09-28 12:54:52 +0100

    change Application

--- src/main/java/webService/Application.java
+++ src/main/java/webService/Application.java
@@ -9,6 +9,7 @@ public class Application {
     public static void main(String[] args) {
         String port = System.getenv("PORT");
         Spark.port(Optional.ofNullable(port).map(Integer::valueOf).orElse(4567));
+        Spark.staticFileLocation("/public");
         new HelloController();
     }
 
